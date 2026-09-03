39f5dbfe6f6e05e7092e580d56ad0fdafc8cd8e7
SimonStockinger <simo.stockinger@gmail.com>
2026-09-03 14:37:36 +0200

    Added initial Ratatui ui widget

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use std::slice::from_mut;
+
 use color_eyre::eyre::{Ok, Result};
 use crossterm::event::{self, Event};
 use ratatui::{
     DefaultTerminal, Frame,
-    widgets::{Paragraph, Widget},
+    layout::{Constraint, Layout},
+    style::{Color, Stylize},
+    widgets::{Block, List, ListItem, Widget},
 };
+
+#[derive(Debug, Default)]
+struct AppState {
+    path: &'static str,
+    data: Data,
+}
 
 #[derive(Debug, Default)]
 struct Data {
@@ -12,8 +22,8 @@ struct Data {
 
 #[derive(Debug, Default)]
 struct CardDeck {
-    title: String,
-    date: String,
+    title: &'static str,
+    date: &'static str,
     index: u32,
     total_number_of_cards: u32,
     cards_left: u32,
@@ -22,25 +32,47 @@ struct CardDeck {
 
 #[derive(Debug, Default)]
 struct FlashCard {
-    title: String,
-    question: String,
-    answer: String,
+    title: &'static str,
+    question: &'static str,
+    answer: &'static str,
     finished: bool,
 }
 
 fn main() -> Result<()> {
+    let mut state = AppState::default();
+    init(&mut state);
+
     color_eyre::install();
     let terminal = ratatui::init();
 
-    let res = run(terminal);
+    let res = run(terminal, &mut state);
     ratatui::restore();
     res
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn init(app_state: &mut AppState) -> Result<()> {
+    let card = FlashCard {
+        title: "TestCard",
+        question: "Why?",
+        answer: "That's why!",
+        finished: false,
+    };
+    let cards = vec![card];
+    app_state.data.decks.push(CardDeck {
+        title: "Test",
+        date: "2026-09-03",
+        index: 0,
+        total_number_of_cards: 1,
+        cards_left: 1,
+        flashcards: cards,
+    });
+    Ok(())
+}
+
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
-        terminal.draw(render);
+        terminal.draw(|f| render(f, app_state));
         // Input handling
         if let Event::Key(key) = event::read()? {
             match key.code {
@@ -54,6 +86,24 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame) {
-    Paragraph::new("text").render(frame.area(), frame.buffer_mut());
+fn render(frame: &mut Frame, app_state: &AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+    let [inner_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(border_area);
+    Block::bordered()
+        .border_type(ratatui::widgets::BorderType::Rounded)
+        .fg(Color::DarkGray)
+        .render(border_area, frame.buffer_mut());
+
+    List::new(
+        app_state
+            .data
+            .decks
+            .iter()
+            .map(|x| ListItem::from(x.title.clone())),
+    )
+    .render(inner_area, frame.buffer_mut())
 }
